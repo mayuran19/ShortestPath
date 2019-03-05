85da65cd9fe179a2d950fc085ba905d393143ba5
mayuran19 <mayuran19@gmail.com>
2019-03-06 01:10:16 +0800

    comments added

--- src/main/java/com/mayuran19/exercise/shortestpath/Graph.java
+++ src/main/java/com/mayuran19/exercise/shortestpath/Graph.java
@@ -6,6 +6,10 @@ public class Graph {
     public Set<Node> graphNodes = new HashSet<>();
     public Set<Edge> graphEdges = new HashSet<>();
 
+    /**
+     * Method to build the graph based on provided Edge array
+     * @param edges
+     */
     public void buildGraph(Edge[] edges){
         for(Edge edge : edges){
             graphEdges.add(edge);
@@ -32,6 +36,12 @@ public class Graph {
         }
     }
 
+    /**
+     * When a path is given as string A-B-C-D, this method will convert it
+     * into list of Edge objects in the order
+     * @param path
+     * @return
+     */
     public List<Edge> getPath(String path){
         List<Edge> edges = new ArrayList<>();
         String[] nodeNames = path.split("-");
@@ -49,6 +59,11 @@ public class Graph {
         return edges;
     }
 
+    /**
+     * Print the distance of a path
+     * Path string will be for example A-B-C-D-E
+     * @param path
+     */
     public void printDistance(String path){
         List<Edge> edges = getPath(path);
         if(edges == null){
@@ -59,6 +74,11 @@ public class Graph {
         }
     }
 
+    /**
+     * Get the Node object using the node name
+     * @param name
+     * @return
+     */
     public Node getNodeByName(String name){
         return graphNodes.stream()
                 .filter(node -> node.getName().equals(name))
@@ -66,6 +86,12 @@ public class Graph {
                 .get();
     }
 
+    /**
+     * Get the Edge object using source node name and destination node name
+     * @param source
+     * @param destination
+     * @return
+     */
     public Edge getEdgeBySourceAndDestination(Node source, Node destination){
         Optional<Edge> edgeOptional = graphEdges.stream()
                 .filter(edge -> edge.equals(new Edge(source, destination, Integer.MAX_VALUE)))
@@ -77,6 +103,12 @@ public class Graph {
         }
     }
 
+    /**
+     * Build the possible paths from source to destination
+     * Currently it just print the paths
+     * @param source
+     * @param destination
+     */
     public void buildAllPaths(String source, String destination){
         graphNodes.stream().forEach(node -> node.setVisited(false));
         Node sourceNode = getNodeByName(source);
@@ -89,8 +121,16 @@ public class Graph {
     }
 
     /**
-     * visit the current node and return the next node to visit
-     * @param node
+     * 1. Visit the source node
+     * 2. Get all outgoing edges for source node
+     * 3. In loop, follow the each edge from the source node
+     * 4. Add the current node to stack
+     * 5. If the current node is same as destination, print the path and pop the stack
+     * 6. If the current node is marked as visited, break the loop
+     * 7. Else recurrsivly visit the nodes
+     * @param source
+     * @param destination
+     * @param stack
      * @return
      */
     public void visitNode(Node source, Node destination, Stack<Edge> stack){
@@ -99,19 +139,24 @@ public class Graph {
             Node edgeDestination = getNodeByName(edge.getDestination().getName());
             stack.add(edge);
             if(edgeDestination.equals(destination)){
-                //Reached destination
+                //Reached destination, print the full path and pop the node
                 stack.stream()
                         .forEach(edge1 -> System.out.println(edge1.getSource().getName() + "->" + edge1.getDestination().getName()));
                 stack.pop();
             } else if(source.isVisited()){
+                //Node has been visited already, break the loop
                 break;
             } else{
+                //Next node, visit recurrsively
                 visitNode(edgeDestination, destination, stack);
             }
         }
+
+        //All the edges are visited, marked the node as visited
         source.setVisited(true);
     }
 
+    //@TODO Since the Node and Edges are fixed, we can use object pooling. To be completed later. Currently not in use
     public static class NodeBuilder{
         private Set<Node> nodes = new HashSet<>();
 
@@ -128,6 +173,7 @@ public class Graph {
         }
     }
 
+    //@TODO Since the Node and Edges are fixed, we can use object pooling. To be completed later. Currently not in use
     private static class EdgeBuilder{
         private Set<Edge> edges = new HashSet<>();
 
--- src/main/java/com/mayuran19/exercise/shortestpath/Path.java
+++
@@ -1,5 +0,0 @@
-package com.mayuran19.exercise.shortestpath;
-
-public class Path {
-
-}
